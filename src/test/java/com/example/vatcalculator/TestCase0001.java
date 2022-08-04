9447bfdb15f7e5233e8a3089ab2ce7dbd3cb2c9c
farrukhaftab <b4farrukh@gmail.com>
2022-08-04 15:05:43 +0500

    tc001 completed

--- src/test/java/com/example/vatcalculator/TestCase0001.java
+++ src/test/java/com/example/vatcalculator/TestCase0001.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class TestCase0001 {
     // This testcase will calculate Final VAT inclusive Price
@@ -16,6 +17,13 @@ public class TestCase0001 {
         WebElement beforeVAT = driver.findElement(By.cssSelector("input[name=beforetax]"));
         beforeVAT.sendKeys("25");
         WebElement vatRate = driver.findElement(By.cssSelector("input[name=taxrate]"));
+        vatRate.clear();
+        vatRate.sendKeys("12");
+        WebElement calcButton = driver.findElement(By.cssSelector("input[type=image]"));
+        calcButton.click();
+        String msg = driver.findElement(By.cssSelector("#content > div:nth-child(6)")).getText();
+        Assert.assertEquals(msg,"Final VAT Inclusive Price: 28.00");
+        driver.quit();
 
     }
 }
